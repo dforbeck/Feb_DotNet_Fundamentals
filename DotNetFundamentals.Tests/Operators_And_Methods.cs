276e359d612262780dd9434ab54a2ebbcdb87c62
Ransford Walker <ransfordw@gmail.com>
2019-02-05 14:44:44 -0500

    Unit Testing: Passing tests for operator methods

--- DotNetFundamentals.Tests/Operators_And_Methods.cs
+++ DotNetFundamentals.Tests/Operators_And_Methods.cs
@@ -1,14 +1,72 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DotNetFundamentals.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class Operators_And_Methods
     {
-        [TestMethod]
-        public void TestMethod1()
+        int a;
+        int b;
+        int x;
+
+        [TestInitialize]
+        public void Arrange()
         {
+            a = 3;
+            b = 9;
+            x = 5;
         }
+
+        [TestMethod]
+        public void AddTwoNumbers_Test()
+        {
+            var expected = 12;
+            var actual = AddTwoNumbers(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SubtractTwoNumbers_Test()
+        {
+            var expected = 6;
+            var actual = SubtractTwoNumbers(b, a);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MultiplyTwoNumbers_Test()
+        {
+            var actual = MultiplyTwoNumbers(a, b);
+            var expected = 27;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DivideTwoNumbers_Test()
+        {
+            var actual = DivideTwoNumbers(b, a);
+            var expected = 3;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetRemainder_Test()
+        {
+            int c = b % x;
+            var expected = 4;
+            var actual = GetRemainder(b, x);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public int AddTwoNumbers(int x, int y) => x + y;
+        public int SubtractTwoNumbers(int x, int y) => x - y;
+        public int MultiplyTwoNumbers(int x, int y) => x * y;
+        public int DivideTwoNumbers(int x, int y) => x / y;
+        public int GetRemainder(int x, int y) => x % y;
     }
 }
