63e707b42dad51298f237e5963cc524920bb5060
Ransford Walker <ransfordw@gmail.com>
2019-02-05 10:27:55 -0500

    Built project, initial commit

--- 01_HelloWorld/Program.cs
+++ 01_HelloWorld/Program.cs
@@ -8,8 +8,16 @@ namespace _01_HelloWorld
 {
     class Program
     {
+        //Challenge: write "Hello World" to the Console
+        //Challenge: write Hello to the console by using a string variable. Output should say "Hello World"
+
+        //Challenge: create and assign two string variables one for first name and one for last name and using interpolation output that to the console as "Hello FirstName LastName".
+
+            //Challenge: Get the user's first name, last name, and age and output to the console "Good Morning, FirstName LastName, your age is unknown."
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
